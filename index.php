76ba46508766e9b9cea7063dda676ef32c8a558d
Md. Josim Uddin <mdjosimuddin712@gmail.com>
2016-05-07 23:29:12 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?php bloginfo('name')|bloginfo('description'); ?></title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+</head>
+<body>
+<div id="page"> 
+    <div id="header" style="background: url(<?php echo get_template_directory_uri()?>/image/header.png) no-repeat 0 0 / 100% 100%;">
+    	<div class="title">A Subtle Beige</div>
+        <div class="subText">A design by Bryant Smith</div>
+    </div>
+    <div id="bar">
+        <div class="menuLink"><a href="index.html">Home</a></div>
+        <div class="menuLink"><a href="index.html">About</a></div>
+        <div class="menuLink"><a href="index.html">Portfolio</a></div>
+        <div class="menuLink"><a href="index.html">Services</a></div>
+        <div class="menuLink"><a href="index.html">Contact</a></div>
+    </div>
+    <div id="pageContent">
+    
+    <div class="articleTitle">The Article Title</div>
+        
+        
+      <div class="articleContent">
+      
+      <div class="rightLinks">
+            <div class="linkTitle">Links:</div>
+    			<p class="links">
+                <a href="http://www.bryantsmith.com">Web Design</a><br />
+                <a href="http://www.bryantsmith.com">Templates</a><br />
+                <a href="http://www.bryantsmith.com">Marketing</a><br />
+                <a href="http://www.bryantsmith.com">SEO</a><br />
+                <a href="http://www.bryantsmith.com">Programming</a><br />
+                <a href="http://www.bryantsmith.com">Consulting</a><br />
+				</p>
+        </div>
+          <p>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer.   This template validates XHTML Strict 1.0, CSS Validates as well; enjoy :) </p><br />
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</p><br />
+          <p>Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue tortor, tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus quis, laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis, pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.</p>
+	</div>
+
+
+    </div>
+
+</div>
+    <div id="footer"><a href="http://www.aszx.net">web development</a> by <a href="http://www.bryantsmith.com">bryant smith</a></div>
+        
+</body>
+</html>
